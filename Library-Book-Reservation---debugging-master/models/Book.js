1de9e8cfc17a73cb41f79f057bf7373ee9f43ec3
hessin-saleh <digilians@outlook.com>
2026-02-28 21:02:49 +0200

    Update Book.js

--- Library-Book-Reservation---debugging-master/models/Book.js
+++ Library-Book-Reservation---debugging-master/models/Book.js
@@ -15,7 +15,9 @@ const bookSchema = new mongoose.Schema({
   },
   addedAt: {
     type: Date,
-    default: Date.now(),
+    // Bug: Using `Date.now()` executes the function immediately when the server starts. Every book will get the exact same timestamp (server start time) instead of the time the book was actually added.
+    // Fix: Removed the parentheses to pass the function reference `Date.now`. This tells Mongoose to execute the function only when a new document is created.
+    default: Date.now,
   },
 });
 
